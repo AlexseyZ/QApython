dc4faec48fe2c2848c12cff39f28aa772d2e820e
AlexseyZ <zverevaa86@mail.ru>
2023-09-11 15:01:36 +0300

    vvod_i_vyvod_dannykh-operatsii_s_chislami_strokami

--- lessons_yandex.py
+++ lessons_yandex.py
@@ -2,7 +2,6 @@
 n = int(input())  # кол-во строк
 print("Купи слона!\n" * n)
 
-
 '''В ожидании доставки'''
 h = int(input())
 m = int(input())
@@ -15,3 +14,20 @@ print(f"{hours // 10}{hours % 10}:{minutes // 10}{minutes % 10}")
 n = int(input())
 c = input()
 print(f'Я больше никогда не буду писать "{c}"!\n' * n)
+
+'''Зайка — 1'''
+a = input()
+if 'зайка' in a:
+    print('YES')
+else:
+    print('NO')
+
+'''Музыкальный инструмент'''
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a < (b + c) and b < (c + a) and c < (a + b):
+    print('YES')
+else:
+    print('NO')
