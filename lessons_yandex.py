ea5b4eb065d5e3c368c8ca8c1fb601ceb6f4802c
AlexseyZ <zverevaa86@mail.ru>
2023-09-12 12:05:05 +0300

    vvod_i_vyvod_dannykh-operatsii_s_chislami_strokami

--- lessons_yandex.py
+++ lessons_yandex.py
@@ -31,3 +31,24 @@ if a < (b + c) and b < (c + a) and c < (a + b):
     print('YES')
 else:
     print('NO')
+
+'''Властелин Чисел: Две Башни'''
+n = int(input())
+a = n // 100
+# print(a)
+b = n // 10 % 10
+# print(b)
+c = n % 10
+# print(c)
+min = min(a, b, c)
+# print(min)
+max = max(a, b, c)
+# print(max)
+d = (a + b + c) - min - max
+# print(d)
+if b == 0 and c == 0 or a == b == c:
+    print()
+elif min == 0:
+    print(d, min, " ", max, d, sep=(""))
+else:
+    print(min, d, " ", max, d, sep=(""))
