900822a7a387d2094444898c30009a48e9d9c118
AlexseyZ <zverevaa86@mail.ru>
2023-09-20 10:42:28 +0300

    методы_работы_со_строками_списками

--- Lesson_3_stroki_kortezhi_spiski.py
+++ Lesson_3_stroki_kortezhi_spiski.py
@@ -4,9 +4,9 @@ for i in s:
     print(i, end='\n')
 
 '''А роза упала на лапу Азора 4.0'''
-# s = str(input())
-# a = s[::-1]
-# if s == a:
-#     print("YES")
-# else:
-#     print("NO")
+s = str(input())
+a = s[::-1]
+if s == a:
+    print("YES")
+else:
+    print("NO")
